4c0b7fcb508340dbef2c254187999f57dcc153bc
Yasin Kavakli <yasin@kavakli.at>
2024-01-12 14:45:23 +0100

    new design testing

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,7 +7,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Switzer-Variable", ...defaultTheme.fontFamily.sans],
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
       },
     },
   },
