ebd1dd915f40b9228b5d3aa642295d9ea1ab516d
Yasin Kavakli <yasin@kavakli.at>
2024-01-16 15:14:32 +0100

    lighthouse perf optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,21 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import metaTags from "astro-meta-tags";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://yasin.kavakli.at",
   output: "server",
   adapter: cloudflare({
-    imageService: "passthrough",
+    imageService: "passthrough"
   }),
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), metaTags(), robotsTxt()],
   markdown: {
     shikiConfig: {
-      theme: "github-dark",
-    },
-  },
+      theme: "github-dark"
+    }
+  }
 });
